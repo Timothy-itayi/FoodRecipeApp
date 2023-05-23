4d00caf9437783db285ac9b4c368e1f3e5843ffc
Timothy itayi <timmytam10@gmail.com>
2023-05-23 16:11:31 +1200

    add auth0 to app

--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,18 @@
 import { createRoot } from 'react-dom/client'
-
-
+import { Auth0Provider } from '@auth0/auth0-react'
+import React from 'react'
 
 import App from './App'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-  
+    <Auth0Provider
+      domain="dev-kuvlvwpp7p78xckw.au.auth0.com"
+      clientId="I9oex4ikfZqEz6Q5WFAKQN8bf9FDQDvL"
+      redirectUri={window.location.origin}
+      audience="https://recipe/api"
+    >
       <App />
-    
+    </Auth0Provider>
   )
 })
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -1,34 +1,33 @@
 import express from 'express'
+import checkJwt from '../Auth0'
 
 const router = express.Router()
 
 import * as db from '../db/usersDb'
 
+// GET /api/v1/users
 router.get('/', async (req, res) => {
   try {
     const users = await db.GetAllUsers()
-    // Get all the Users from db
-
-    res.json({ users: users })
-    // 'send' the users as a response
+    res.json({ users })
   } catch (error) {
     res.status(500).json({ error: (error as Error).message })
   }
 })
 
+// GET /api/v1/users/:id
 router.get('/:id', async (req, res) => {
   try {
-    const userId = await Number(req.params.id)
-
-    // get the user by id
-    const user = db.getUser(userId, res)
-    // json response
+    const userId = Number(req.params.id)
+    const user = await db.getUser(userId, res)
+    res.json({ user })
   } catch (error) {
     res.status(500).json({ error: (error as Error).message })
   }
 })
 
-router.post('/', async (req, res) => {
+// POST /api/v1/users
+router.post('/', checkJwt, async (req, res) => {
   try {
     const newUser = req.body
     await db.addNewUser(newUser)
@@ -38,7 +37,8 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
+// DELETE /api/v1/users/:id
+router.delete('/:id', checkJwt, async (req, res) => {
   try {
     const userId = Number(req.params.id)
     await db.deleteUser(userId)
