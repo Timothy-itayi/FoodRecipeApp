c19302dbbda0b6ab037f1b47d9e1b81f55839509
Timothy itayi <timmytam10@gmail.com>
2023-06-29 16:05:12 +1200

    write test for authentication, mainfeed, postContainer and postFetcher

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -3,24 +3,37 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Post } from './types'
 
 const useIsAuthenticated = () => {
-  const { isAuthenticated } = useAuth0()
-  return isAuthenticated
+  const { isAuthenticated, isLoading } = useAuth0()
+
+  // Check if there is a stored authentication state in session storage
+  const storedIsAuthenticated = sessionStorage.getItem('isAuthenticated')
+
+  // Return true if there is a stored authentication state or if the user is currently authenticated
+  return storedIsAuthenticated === 'true' || (isAuthenticated && !isLoading)
 }
 
 interface AuthenticatedProps {
   children: React.ReactNode
-  posts: Post[] // Add the posts prop with the appropriate type
+  posts: Post[]
 }
 
 export function IfAuthenticated(props: AuthenticatedProps) {
   const { children, posts } = props
   const isAuthenticated = useIsAuthenticated()
+
+  // Store the current authentication state in session storage
+  sessionStorage.setItem('isAuthenticated', String(isAuthenticated))
+
   return isAuthenticated ? <>{children}</> : null
 }
 
 export function IfNotAuthenticated(props: AuthenticatedProps) {
-  const { children } = props
+  const { children, posts } = props
   const isAuthenticated = useIsAuthenticated()
+
+  // Store the current authentication state in session storage
+  sessionStorage.setItem('isAuthenticated', String(isAuthenticated))
+
   return !isAuthenticated ? <>{children}</> : null
 }
 
