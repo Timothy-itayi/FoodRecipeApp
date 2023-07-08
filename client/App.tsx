06c374c8fd2f48da437fb68baed047336eed67b3
Timothy itayi <timmytam10@gmail.com>
2023-07-08 19:24:51 +1200

    updated UserProfile route

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { User, useAuth0 } from '@auth0/auth0-react'
-
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from 'react-router-dom'
+
 import Header from './components/AdminComponents/Header'
 import Footer from './components/AdminComponents/Footer'
-
 import UserProfile from './components/AdminComponents/UserProfile'
 import { IfAuthenticated } from './components/AdminComponents/Authenticated'
 import CreateUser from './components/AdminComponents/CreateUser'
 import Nav from './components/AdminComponents/Nav'
 import MainFeed from './components/AdminComponents/MainFeed'
+import CreatedUser from './components/AdminComponents/CreatedUser'
+import UserPosts from './components/AdminComponents/UserPosts'
 
 interface CustomUser {
   name: string
@@ -56,31 +57,55 @@ const App = () => {
         <Route
           path="/create-user"
           element={
-            <CreateUser
-              selectedIcon={selectedIcon}
-              onCreateUser={(username: string, userEmail: string) => {
-                throw new Error('Function not implemented.')
-              }}
-            />
+            isAuthenticated ? (
+              <CreateUser
+                selectedIcon={selectedIcon}
+                onCreateUser={(username: string, userEmail: string) => {
+                  throw new Error('Function not implemented.')
+                }}
+              />
+            ) : (
+              <Navigate to="/" />
+            )
           }
         />
         <Route
-          path="/mainfeed"
+          path="/created-user"
           element={
-            <MainFeed
-              posts={[]}
-              handleDeletePost={(id: number) => {
-                throw new Error('Function not implemented.')
-              }}
-            />
+            isAuthenticated ? (
+              <CreatedUser isAuthenticated={isAuthenticated} />
+            ) : (
+              <Navigate to="/" />
+            )
           }
-        />
+        >
+          <Route
+            path="mainfeed"
+            element={
+              <MainFeed
+                posts={[]}
+                handleDeletePost={function (id: number): void {
+                  throw new Error('Function not implemented.')
+                }}
+              />
+            }
+          />
+          <Route
+            path="userposts"
+            element={
+              <UserPosts
+                handleCreatePost={function (): void {
+                  throw new Error('Function not implemented.')
+                }}
+              />
+            }
+          />
+        </Route>
       </Routes>
       <Footer />
     </>
   )
 }
-
 const Home = () => {
   return (
     <IfAuthenticated>
@@ -88,5 +113,4 @@ const Home = () => {
     </IfAuthenticated>
   )
 }
-
 export default App
