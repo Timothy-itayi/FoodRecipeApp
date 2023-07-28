0f2afbb626e27286b1a2e8b3598eeeb64d3e8fa0
Timothy itayi <timmytam10@gmail.com>
2023-07-29 10:05:42 +1200

    add new post/ update css for cardComponent in postContainer

--- client/components/AdminComponents/BodyComponents/PostContainer.tsx
+++ client/components/AdminComponents/BodyComponents/PostContainer.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Card, CardContent, Typography } from '@material-ui/core'
 import { getAllPosts } from '../../../apis/posts'
 import { Post } from '../../types'
+
 interface PostContainerProps {
   posts: Post[]
 }
@@ -34,22 +35,34 @@ const PostContainer: React.FC<PostContainerProps> = () => {
     fetchData()
   }, [])
 
-  // console.log('State Data:', blogs) // Use blogs instead of posts
-  // console.log('Is blogs an array?', Array.isArray(blogs)) // Use blogs instead of posts
+  // Function to determine if an image is larger
+  const isImageLarge = (image_url: string) => {
+    // Adjust the threshold as needed based on image sizes
+    const thresholdWidth = 400
+    const thresholdHeight = 200
+    const img = new Image()
+    img.src = image_url
+    return img.width >= thresholdWidth || img.height >= thresholdHeight
+  }
 
   return (
-    <>
+    <div className="post-container">
       <h1>Post something</h1>
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
         <p>Error: {error}</p>
       ) : (
-        <div>
+        <>
           {Array.isArray(blogs) && blogs.length > 0 ? (
             blogs.map((blog) => (
-              <Card key={blog.id} style={{ marginBottom: '3px' }}>
-                <CardContent>
+              <Card
+                key={blog.id}
+                style={{ marginBottom: '15px', height: 'auto' }}
+              >
+                <CardContent
+                  style={{ display: 'flex', flexDirection: 'column' }}
+                >
                   <Typography
                     variant="h6"
                     component="h2"
@@ -57,29 +70,33 @@ const PostContainer: React.FC<PostContainerProps> = () => {
                   >
                     {blog.title}
                   </Typography>
-                  <Typography variant="body1" style={{ marginBottom: '3px' }}>
+                  <Typography
+                    variant="body1"
+                    style={{ marginBottom: '3px', flex: 1 }}
+                  >
                     {blog.description}
                   </Typography>
                   {blog.image_url && (
-                    <img
-                      src={blog.image_url}
-                      alt={blog.title}
+                    <div
+                      className="card-image"
                       style={{
-                        marginBottom: '3px',
-                        maxWidth: '100%',
-                        height: '50',
+                        maxHeight: isImageLarge(blog.image_url)
+                          ? '150px'
+                          : 'auto',
                       }}
-                    />
+                    >
+                      <img src={blog.image_url} alt={blog.title} />
+                    </div>
                   )}
                 </CardContent>
               </Card>
             ))
           ) : (
-            <p>No posts found</p>
+            <p>No blogs to display</p>
           )}
-        </div>
+        </>
       )}
-    </>
+    </div>
   )
 }
 
